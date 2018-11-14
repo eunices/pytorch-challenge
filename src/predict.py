c1bab8a36eeebe1d6c8504f2b7840357360c18ec
Eunice Soh <t0915817@kelaberetiv.sg>
2018-11-14 13:09:49 +0800

    First commit by Eunice to initialize environment

---
+++ src/predict.py
@@ -0,0 +1,5 @@
+"""Script to predict using pytorch model."""
+
+
+if __name__ == '__main__':
+    pass
---
+++ src/train.py
@@ -0,0 +1,9 @@
+"""Script to train pytorch model."""
+import torchvision.models as models
+
+LEARNING_RATE = 0.1
+HIDDEN_UNITS = 10
+TRAINING_EPOCHES = 10
+
+if __name__ == '__main__':
+    print('hi')
